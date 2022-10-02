a86d7e3ae833be7e00d1d018ab2e21591c127743
Warley Serafim <80359681+warleyserafim@users.noreply.github.com>
2022-10-01 23:25:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://152.67.43.35:3000",
         methods: ["GET", "POST"]
     }
 });
